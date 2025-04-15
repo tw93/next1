2ac877e01d03aee19dafb1ed8e471406893eca8d
侑夕 <tw102972@alibaba-inc.com>
2025-04-15 23:01:09 +0800

    :bug: update

--- src/pages/api/proxy-fai.js
+++ src/pages/api/proxy-fai.js
@@ -4,7 +4,16 @@ export default async function handler(req, res) {
   const { method, headers, body } = req;
   headers.delete('host');
   headers.delete('referer');
+
+  // 设置必要的请求头
   headers.set('Accept-Encoding', 'identity');
+  headers.set('Accept', 'text/event-stream');
+  headers.set('Cache-Control', 'no-cache');
+  headers.set('Connection', 'keep-alive');
+
+  // 设置其他优化头
+  headers.set('X-Accel-Buffering', 'no'); // 禁用Nginx缓冲
+  headers.set('Transfer-Encoding', 'chunked'); // 启用分块传输编码
 
   const url = `https://pre-api.alitrip.alibaba.com/fai/ctstream001`;
   const options = {
@@ -12,6 +21,9 @@ export default async function handler(req, res) {
     method: method,
     body: body,
     redirect: 'follow',
+    // 添加fetch选项以优化连接
+    keepalive: true, // 保持连接
+    cache: 'no-store', // 禁用缓存
   };
   const modifiedRequest = new Request(url, options);
   try {
